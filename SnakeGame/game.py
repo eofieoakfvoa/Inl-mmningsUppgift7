ece0b7821751afcb6cdf3c91eb8e47faf4547f04
eofieoakfvoa <richard.nilsson2005@gmail.com>
2025-05-04 15:42:36 +0200

    :cat:

--- SnakeGame/game.py
+++ SnakeGame/game.py
@@ -1,3 +1,11 @@
 import pygame
 class GameSystem():
-    Resolution = pygame.Vector2(400, 300) 
+        
+    def __init__(self):
+        GameSystem.Initialize()
+    Resolution = pygame.Vector2(400, 300) #använder vector2 eftersom det är lättare att använda .x och .y 
+    Title = "Snake Game"
+    GameDisplay = None 
+    def Initialize():
+        GameDisplay = pygame.display.set_mode((GameSystem.Resolution.x, GameSystem.Resolution.y))
+        pygame.display.set_caption("Snake game")
--- SnakeGame/gameloop.py
+++ SnakeGame/gameloop.py
@@ -1,10 +1,8 @@
 import pygame
 from game import *
-Game = GameSystem()
 pygame.init()
-dis=pygame.display.set_mode((400,300))
+Game = GameSystem()
 pygame.display.update()
-pygame.display.set_caption("Snake game")
 game_over = False
 while not game_over:
     for event in pygame.event.get():
