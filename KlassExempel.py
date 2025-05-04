d7f99427b9c18beef7c50b379a85d255f3ddf331
eofieoakfvoa <richard.nilsson2005@gmail.com>
2025-05-04 15:08:15 +0200

    :cat:

---
+++ KlassExempel.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.Name = name
+        self.Age = age
+        self.Gender = gender
+    def GetInformation(self):
+        pass
