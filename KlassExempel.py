7e68c68f003756c6dd479a5c85d16dbd6afec3bd
eofieoakfvoa <richard.nilsson2005@gmail.com>
2025-05-04 15:16:00 +0200

    :cat:

--- KlassExempel.py
+++ KlassExempel.py
@@ -1,7 +1,15 @@
 class Person:
-    def __init__(self, name, age, gender):
+    def __init__(self, name : str, age : float, gender : int):
         self.Name = name
         self.Age = age
         self.Gender = gender
     def GetInformation(self):
-        pass
+        return (self.Name, self.Age, self.Gender)
+    def IsOfAge(self):
+        if self.Age >= 18: 
+            return True
+        return False
+
+#test
+egkjimsavd = Person("egkjimsavd", 132.5, "man")
+print(egkjimsavd.GetInformation())
